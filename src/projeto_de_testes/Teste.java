9415d5635f316e89b54e2be99a919766db102571
Vitor Alfeu Eliotério <velioterio@dxc.com>
2020-01-02 10:51:08 -0300

    Subindo primeira versão do projeto

---
+++ src/projeto_de_testes/Teste.java
@@ -0,0 +1,34 @@
+package projeto_de_testes;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Teste {
+	public static void main(String[] args){
+//		TESTANDO SUBSTRING EM JAVA
+//		
+		String phone = "Sucesso :: CustomerOrderManagement#109501682 :: BA_ONLINE_EDITION";		
+		System.out.println(phone.substring(0,34));
+		
+//		//TESTANDO SE EXISTE DETERMINADA STRING EM UM VETOR DE STRING
+//		String[] nomes = {"joao", "maria", "jose"};
+//		List<String> nomess = Arrays.asList(nomes);
+//		
+//		if (nomess.contains("joao")) {
+//			System.out.println("foi");
+//		} else {
+//			System.out.println("não foi");
+//		}
+		
+//		String teste = "TDM", teste2 = "Tdm";
+//		if (teste.toUpperCase().contains(teste2.toUpperCase())) {
+//			System.out.println("Sim");
+//		} else {
+//			System.out.println("Não");
+//		}
+		
+		//COLOCANDO STRING DENTRO DE STRING
+		System.out.println("O campo \"asdasdsadasd\" não pode ser vazio.");
+	}
+}
